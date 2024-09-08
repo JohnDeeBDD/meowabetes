97609047113c2b07d264804af231311280790c90
masterasia1 <a.mayfield18@gmail.com>
2024-09-08 14:52:37 -0700

    Start

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Meowabetes
+Plugin URI: https://meowabetes.com
+Description: Meow
+Version: 1
+Author: AsiaMayfield
+Author URI: https://generalchicken.guru
+License: Copyright(C) 2024, generalchicken.guru . All rights reserved. THIS IS NOT FREE SOFTWARE.
+*/
